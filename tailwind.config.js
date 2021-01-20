8a9c2ecdb8020b4b15427beb6e8bf4476f5ce2ee
Soyun Park <55320813+soyunparrrk@users.noreply.github.com>
2021-01-20 19:27:01 +0100

    big update

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+        textColor: {
+       'secondary': '#9190BE',
+     },
+    extend: {
+        fontFamily: {
+        mainSerif: ['MingLiU'],
+        mainSans: ['suisse_regular']
+        }
+        
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
